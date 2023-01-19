fb4400cd079a0cf204fb05af67c45d7f7c669914
Antoine LRK <admin@antoinelrk.com>
2023-01-19 11:23:38 +0100

    Clean code, compare Set/Array

---
+++ assets/app.js
@@ -0,0 +1,46 @@
+(async () => {
+    /**
+     * On request les travaux à l'API
+     */
+    let works = await fetch('http://localhost:5678/api/works').then(response => response.json())
+    let categories = await fetch('http://localhost:5678/api/categories').then(response => response.json())
+
+    let filtersContainer = document.querySelector('.filters-container')
+
+    categories.forEach((category) => {
+        let li = document.createElement('li')
+        li.innerHTML = `
+        <li>
+            <button class="filter-btn" data-id="${category.id}">${category.name}</button>
+        </li>`
+        filtersContainer.appendChild(li)
+    })
+
+    changeArrayForFilter(works, {id: 0})
+
+    const buttonFilter = document.querySelectorAll('button.filter-btn')
+    buttonFilter.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            buttonFilter.forEach((e2) => e2.classList.remove('active'))
+            changeArrayForFilter(works, {
+                id: parseInt(e.target.getAttribute('data-id'))
+            })
+            e.target.classList.add('active')
+        })
+    })
+})();
+
+const changeArrayForFilter = (works, filter) => {
+    let filtered = works.filter(element => filter.id !== 0 ? element.categoryId === filter.id : element)
+
+    let gallery = document.querySelector('.gallery')
+    gallery.innerHTML = ''
+    filtered.forEach((e) => {
+        let figureElement = document.createElement('figure')
+        figureElement.innerHTML = `
+            <img crossorigin="anonymous" src="${e.imageUrl}" alt="${e.title}">
+            <figcaption>${e.title}</figcaption>
+        `
+        gallery.appendChild(figureElement)
+    })
+}
