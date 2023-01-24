4f2bc0a4ddf1b58c49908499308fbe8de20da2fd
Antoine LRK <admin@antoinelrk.com>
2023-01-24 11:12:17 +0100

    Added auth interface remover on logout

--- assets/app.js
+++ assets/app.js
@@ -15,6 +15,8 @@ authLink.addEventListener('click', (e) => {
         e.preventDefault()
         SessionManager().unvalidate()
         authLink.innerText = `login`
+        const authComponents = document.querySelectorAll('.auth-component')
+        authComponents.forEach((component) => component.remove());
     } else {
         if (authLink.getAttribute('href') === null) authLink.setAttribute('href', 'login.html')
     }
