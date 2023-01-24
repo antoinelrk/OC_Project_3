790f3c5bcd2fde82725bfc8f97afc0c2f726cc5d
Antoine LRK <admin@antoinelrk.com>
2023-01-24 10:29:29 +0100

    Fix login btn listener

--- assets/app.js
+++ assets/app.js
@@ -3,14 +3,19 @@ import { SessionManager } from './libs.js'
 /**
  * Authenticated
  */
+let authLink = document.querySelector('#authLink')
+
 if (SessionManager().isAuthenticated()) {
-    let authLink = document.querySelector('#authLink')
     authLink.removeAttribute('href')
     authLink.innerText = `logout`
-    authLink.addEventListener('click', (e) => {
+}
+
+authLink.addEventListener('click', (e) => {
+    if (SessionManager().isAuthenticated()) {
         e.preventDefault()
         SessionManager().unvalidate()
         authLink.innerText = `login`
-        authLink.setAttribute('href', 'login.html')
-    })
-}
+    } else {
+        if (authLink.getAttribute('href') === null) authLink.setAttribute('href', 'login.html')
+    }
+})
