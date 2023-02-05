96e9b705ddcefe30136337a0a5e377580d53c3ff
Antoine LRK <admin@antoinelrk.com>
2023-02-05 16:21:00 +0100

    Finishing website complete prototype

--- assets/index.js
+++ assets/index.js
@@ -16,7 +16,6 @@ const loadModal = async (url) => {
     document.querySelector('#app').prepend(element)
     return element
 }
-
 const openModal = async (e) => {
     e.preventDefault()
     const target = e.target.getAttribute('href')
@@ -37,7 +36,6 @@ const openModal = async (e) => {
     })
     modal.querySelector('.js-modal-stop')?.addEventListener('click', stopPropagation)
 }
-
 const closeModal = (e) => {
     if (modal === null) return
     if (previouslyFocusedElement !== null) previouslyFocusedElement.focus()
@@ -58,12 +56,10 @@ const closeModal = (e) => {
     }
     modal.addEventListener('animationend', hideModal)
 }
-
 window.addEventListener('keydown', (e) => {
     if (e.key === "Escape" || e.key === "Esc") closeModal(e)
     if (e.key === "Tab" && modal !== null) focusInModal(e)
 })
-
 const focusInModal = (e) => {
     e.preventDefault()
     let index = focusables.findIndex(f => f === modal.querySelector(':focus'))
@@ -80,7 +76,6 @@ const focusInModal = (e) => {
     }
     focusables[index].focus()
 }
-
 const stopPropagation = (e) => {
     e.stopPropagation()
 }
@@ -95,11 +90,8 @@ const updateCategories = (works) => {
             localCategories.push(work.category)
         }
     })
-
     let filtersContainer = document.querySelector('.filters-container')
-    let filterPattern = `
-        <li><button data-id="0" class="filter-btn active">Tous</button></li>
-    `
+    let filterPattern = `<li><button data-id="0" class="filter-btn active">Tous</button></li>`
     localCategories.forEach((category) => {
         filterPattern += `<li><button class="filter-btn" data-id="${category.id}">${category.name}</button></li>`
     })
@@ -125,7 +117,6 @@ const applyFilterListener = (works) => {
     SessionManager().refreshHUD(works);
     updateCategories(works)
     changeArrayForFilter(works, {id: 0})
-    applyFilterListener(works)
 
     if (SessionManager().isAuthenticated()) {
         /**
@@ -140,54 +131,20 @@ const applyFilterListener = (works) => {
         portfolioEditModal.setAttribute('aria-hidden', 'true') // TODO: CHANGER POUR PRODUCTION (true)
         portfolioEditModal.setAttribute('role', 'dialog')
         portfolioEditModal.setAttribute('aria-labelledby', 'titlemodal')
-        let worksLoop = ``
-        const worksLooped = () => {
-            if (worksLoop !== ``) worksLoop = ``
-            works.forEach((work) => {
-                worksLoop += `
-                    <div class="work">
-                        <img crossorigin="anonymous" src="${work.imageUrl}" alt=""/>
-                        <div class="controls">
-                            <button class="js-work-move">
-                                <figure>
-                                    <svg width="100%" height="100%" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M11.2364 5.81583L9.97332 4.55272C9.82886 4.40818 9.61143 4.36503 9.42271 4.44318C9.23391 4.52139 9.1108 4.7056 9.1108 4.90996V5.66783H6.33199V2.88898H7.08986C7.29421 2.88898 7.47843 2.76587 7.55664 2.57707C7.63482 2.38828 7.5916 2.17096 7.4471 2.02646L6.18399 0.763343C5.98671 0.566028 5.66679 0.566028 5.46947 0.763343L4.20636 2.02646C4.06186 2.17096 4.01865 2.38828 4.09683 2.57707C4.17504 2.76587 4.35928 2.88898 4.5636 2.88898H5.3215V5.66783H2.54266V4.90996C2.54266 4.7056 2.41955 4.52136 2.23076 4.44318C2.04193 4.365 1.82461 4.40822 1.68014 4.55272L0.417029 5.81583C0.219714 6.01314 0.219714 6.33303 0.417029 6.53035L1.68014 7.79346C1.77681 7.89013 1.90598 7.94146 2.03752 7.94146C2.10259 7.94146 2.16824 7.92887 2.23076 7.903C2.41955 7.82479 2.54266 7.64054 2.54266 7.43622V6.67832H5.3215V9.45716H4.56364C4.35928 9.45716 4.17504 9.58028 4.09686 9.76907C4.01868 9.95786 4.0619 10.1752 4.2064 10.3197L5.46951 11.5828C5.56813 11.6815 5.69744 11.7308 5.82675 11.7308C5.95606 11.7308 6.08537 11.6815 6.18399 11.5828L7.4471 10.3197C7.5916 10.1752 7.63482 9.95783 7.55664 9.76904C7.47843 9.58024 7.29421 9.45713 7.08986 9.45713H6.33199V6.67832H9.11084V7.43618C9.11084 7.64054 9.23391 7.82479 9.42274 7.90296C9.48522 7.92887 9.55091 7.94143 9.61598 7.94143C9.74745 7.94143 9.87669 7.8901 9.97332 7.79343L11.2364 6.53031C11.4338 6.33303 11.4338 6.01314 11.2364 5.81583Z" />
-                                    </svg>                            
-                                </figure>
-                            </button>
-                            <button data-id="${work.id}" class="js-work-delete">
-                                <figure>
-                                    <svg width="100%" height="100%" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.6 1.8V0.9C6.6 0.402944 6.19704 0 5.7 0H3.3C2.80294 0 2.4 0.402944 2.4 0.9V1.8H0V2.4H0.6V8.1C0.6 8.59704 1.00294 9 1.5 9H7.5C7.99704 9 8.4 8.59704 8.4 8.1V2.4H9V1.8H6.6ZM3 0.9C3 0.734316 3.13432 0.6 3.3 0.6H5.7C5.86568 0.6 6 0.734316 6 0.9V1.8H3V0.9ZM4.2 4.2V7.2H4.8V4.2H4.2ZM2.4 7.2V5.4H3V7.2H2.4ZM6 5.4V7.2H6.6V5.4H6Z" />
-                                    </svg>
-                                </figure>
-                            </button>
-                        </div>
-                        <button class="work-edit">éditer</span>
-                    </div>
-                `
-            })
+
+        let categoriesLoop = []
+        const categoriesLooped = async () => {
+            let categories = await fetch(`${API_URL}/categories`).then(response => response.json())
+            categoriesLoop = categories
+
+            // return categoriesLoop
         }
-        worksLooped()
-        const listOfPicturesPattern = `
-        <div class="modal-section loop-picture-section">
-            <div class="modal-header">
-                <button class="js-modal-close modal-right-btn">
-                    <figure>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
-                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"></path>
-                        </svg>
-                    </figure>
-                </button>
-            </div>
-            <div class="modal-body">
-                <h3 class="modal-title">Galerie photo</h3>
-                <div class="modal-works-wrapper">${worksLoop}</div>
-                <div class="bar"></div>
-                <a class="add-picture-modal-link js-add-picture" href="">Ajouter une photo</a>
-                <a class="remove-gallery" href="">Supprimer la galerie</a>
-            </div>
-        </div>`
+        await categoriesLooped()
+        let categoriesPattern = ``
+        categoriesLoop.forEach((category) => {
+            categoriesPattern += `<option class="option-category" value="${category.id}">${category.name}</option>`
+        })
+
         const addNewPictureFormPattern = `
         <div class="modal-section add-picture-section">
             <div class="modal-header">
@@ -236,9 +193,7 @@ const applyFilterListener = (works) => {
                         <label class="text-label" for="category">Catégorie</label>
                         <select class="category-selector" name="category" id="category">
                             <option value="" disabled selected>— Selectionner une categorie —</option>
-                            <option class="option-category" value="1">Bar</option>
-                            <option class="option-category" value="1">Bar2</option>
-                            <option class="option-category" value="1">Bar3</option>
+                            ${categoriesPattern}
                         </select>
                     </div>
                     <div class="form-group bar"></div>
@@ -246,9 +201,66 @@ const applyFilterListener = (works) => {
                 </form>
             </div>
         </div>`
-        const portfolioEditModalPattern = `<div class="modal-wrapper js-modal-stop">${listOfPicturesPattern}${addNewPictureFormPattern}</div>`
+        const portfolioEditModalPattern = `
+        <div class="modal-wrapper js-modal-stop">
+            <div class="modal-section loop-picture-section">
+                <div class="modal-header">
+                    <button class="js-modal-close modal-right-btn">
+                        <figure>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+                                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"></path>
+                            </svg>
+                        </figure>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h3 class="modal-title">Galerie photo</h3>
+                    <div class="modal-works-wrapper"></div>
+                    <div class="bar"></div>
+                    <a class="add-picture-modal-link js-add-picture" href="">Ajouter une photo</a>
+                    <a class="remove-gallery" href="">Supprimer la galerie</a>
+                </div>
+            </div>
+            ${addNewPictureFormPattern}
+        </div>
+        `
         portfolioEditModal.innerHTML = portfolioEditModalPattern
         document.querySelector('#app').prepend(portfolioEditModal)
+
+        const refreshWorkLoop = (works) => {
+            const modalWorkWrapper = document.querySelector('.modal-works-wrapper')
+            modalWorkWrapper.innerHTML = ``
+            let worksLoop = ``
+            works.forEach((work) => {
+                worksLoop += `
+                    <div class="work">
+                        <img crossorigin="anonymous" src="${work.imageUrl}" alt=""/>
+                        <div class="controls">
+                            <button class="js-work-move">
+                                <figure>
+                                    <svg width="100%" height="100%" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.2364 5.81583L9.97332 4.55272C9.82886 4.40818 9.61143 4.36503 9.42271 4.44318C9.23391 4.52139 9.1108 4.7056 9.1108 4.90996V5.66783H6.33199V2.88898H7.08986C7.29421 2.88898 7.47843 2.76587 7.55664 2.57707C7.63482 2.38828 7.5916 2.17096 7.4471 2.02646L6.18399 0.763343C5.98671 0.566028 5.66679 0.566028 5.46947 0.763343L4.20636 2.02646C4.06186 2.17096 4.01865 2.38828 4.09683 2.57707C4.17504 2.76587 4.35928 2.88898 4.5636 2.88898H5.3215V5.66783H2.54266V4.90996C2.54266 4.7056 2.41955 4.52136 2.23076 4.44318C2.04193 4.365 1.82461 4.40822 1.68014 4.55272L0.417029 5.81583C0.219714 6.01314 0.219714 6.33303 0.417029 6.53035L1.68014 7.79346C1.77681 7.89013 1.90598 7.94146 2.03752 7.94146C2.10259 7.94146 2.16824 7.92887 2.23076 7.903C2.41955 7.82479 2.54266 7.64054 2.54266 7.43622V6.67832H5.3215V9.45716H4.56364C4.35928 9.45716 4.17504 9.58028 4.09686 9.76907C4.01868 9.95786 4.0619 10.1752 4.2064 10.3197L5.46951 11.5828C5.56813 11.6815 5.69744 11.7308 5.82675 11.7308C5.95606 11.7308 6.08537 11.6815 6.18399 11.5828L7.4471 10.3197C7.5916 10.1752 7.63482 9.95783 7.55664 9.76904C7.47843 9.58024 7.29421 9.45713 7.08986 9.45713H6.33199V6.67832H9.11084V7.43618C9.11084 7.64054 9.23391 7.82479 9.42274 7.90296C9.48522 7.92887 9.55091 7.94143 9.61598 7.94143C9.74745 7.94143 9.87669 7.8901 9.97332 7.79343L11.2364 6.53031C11.4338 6.33303 11.4338 6.01314 11.2364 5.81583Z" />
+                                    </svg>                            
+                                </figure>
+                            </button>
+                            <button data-id="${work.id}" class="js-work-delete">
+                                <figure>
+                                    <svg width="100%" height="100%" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.6 1.8V0.9C6.6 0.402944 6.19704 0 5.7 0H3.3C2.80294 0 2.4 0.402944 2.4 0.9V1.8H0V2.4H0.6V8.1C0.6 8.59704 1.00294 9 1.5 9H7.5C7.99704 9 8.4 8.59704 8.4 8.1V2.4H9V1.8H6.6ZM3 0.9C3 0.734316 3.13432 0.6 3.3 0.6H5.7C5.86568 0.6 6 0.734316 6 0.9V1.8H3V0.9ZM4.2 4.2V7.2H4.8V4.2H4.2ZM2.4 7.2V5.4H3V7.2H2.4ZM6 5.4V7.2H6.6V5.4H6Z" />
+                                    </svg>
+                                </figure>
+                            </button>
+                        </div>
+                        <button class="work-edit">éditer</span>
+                    </div>
+                `
+            })
+            modalWorkWrapper.innerHTML = worksLoop
+
+        }
+
+        refreshWorkLoop(works)
+
         const addPicturebutton = document.querySelector('.js-add-picture')
         addPicturebutton?.addEventListener('click', (e) => {
             e.preventDefault()
@@ -262,7 +274,8 @@ const applyFilterListener = (works) => {
         document.querySelectorAll('.js-modal').forEach(a => a.addEventListener('click', openModal))
 
         /**
-         * Fonction d'ajout de travaux (works)
+         * Quand l'input du formulaire d'envoi change
+         * TODO: Vérifier que le fichier soit bien une image !
          */
         document.querySelector('.js-image-changer').addEventListener('change', (e) => {
             let loadedImage = document.querySelector('.loaded-img');
@@ -286,12 +299,60 @@ const applyFilterListener = (works) => {
 
         })
 
+        /**
+         * Fonction d'ajout de travaux (works)
+         */
         const addWork = async (e) => {
             /**
-             * 
+             * TODO: Vérifier que tout les inputs ne soit pas null
              */
             e.preventDefault()
-            console.log(e)
+            const image = e.target.image.files[0]
+            const data = {
+                image: image,
+                title: e.target.title.value,
+                category: parseInt(e.target.category.value)
+            }
+
+            const formData = new FormData()
+            formData.append('image', data.image)
+            formData.append('title', data.title)
+            formData.append('category', data.category)
+
+            await fetch(`${API_URL}/works`, {
+                method: 'POST',
+                headers: {
+                    "accept": "*/*",
+                    "Authorization": `Bearer ${SessionManager().getToken()}`
+                },
+                body: formData
+            })
+
+            /**
+             * TODO: Redraw le tableau de la page principale
+             */
+            .then(response => response.json()).then((data) => {
+                let clonedWorks = works
+                const currentCategory = categoriesLoop.filter((category) => category.id === parseInt(e.target.category.value))
+
+                clonedWorks = Array.from(clonedWorks.add({
+                    category: currentCategory[0],
+                    categoryId: parseInt(data.categoryId),
+                    id: data.id,
+                    imageUrl: data.imageUrl,
+                    title : data.title,
+                    userId : data.userId
+                }))
+
+
+                works = new Set(clonedWorks)
+                changeArrayForFilter(works, {id: 0})
+                updateCategories(works)
+                refreshWorkLoop(works)
+                document.querySelector('.modal-wrapper').classList.remove('slided')
+                document.querySelectorAll('.js-work-delete').forEach((element) => element.addEventListener('click', removeWork))
+                resetAddworkForm()
+            })
         }
         
         /**
@@ -304,16 +365,15 @@ const applyFilterListener = (works) => {
                 method: 'DELETE',
                 headers: {
                     "accept": "*/*",
-                    "Authorization": `Bearer ${SessionManager().getToken()}`
+                    "Authorization": `Bearer ${SessionManager().getToken()}`,
                 }
             })
             .then(() => {
-                works = [...works].filter(work => work.id !== workId)
-                worksLooped()
-                let temp = document.querySelector('.modal-works-wrapper')
-                temp.innerHTML = ``
-                temp.innerHTML = worksLoop
+                let clonedWorks = works
+                clonedWorks = [...clonedWorks].filter(work => work.id !== workId)
+                works = new Set(clonedWorks)
                 changeArrayForFilter(works, {id: 0})
+                refreshWorkLoop(works)
                 updateCategories(works)
                 document.querySelectorAll('.js-work-delete').forEach((element) => element.addEventListener('click', removeWork))
                 // if (works.length === 0) closeModal(e)
@@ -338,14 +398,22 @@ const cleanThumb = () => {
 
 }
 
+const resetAddworkForm = () => {
+    cleanThumb()
+    document.querySelector('#title').value = ``
+    document.querySelector('#category').value = ``
+    document.querySelector('#image').value = ``
+}
+
 /**
  * Fonction de mise a jour du tableau de travaux
  */
 const changeArrayForFilter = (works, filter) => {
-    let filtered = [...works].filter(element => filter.id !== 0 ? element.categoryId === filter.id : element)
+    let localWorks = works
+    const filtered = [...localWorks].filter(element => filter.id !== 0 ? element.categoryId === filter.id : element)
 
     let gallery = document.querySelector('.gallery')
-    gallery.innerHTML = ''
+    gallery.innerHTML = ``
     filtered.forEach((e) => {
         let figureElement = document.createElement('figure')
         figureElement.innerHTML = `
